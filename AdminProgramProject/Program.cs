95d7f7d2bb6a83fee377b22f23f148554d1a58a4
Lychee304 <2173013@talnet.nl>
2026-02-18 11:37:18 +0100

    added some work from another project
    it has the completed constructor which saves me the hassle of inplementing it

--- AdminProgramProject/Program.cs
+++ AdminProgramProject/Program.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AdminProgramProject
+namespace AdminProgramLessons
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            User amir = new User(0, "amir", "naast de buren", "hey@gmail.com", "+420 69 69 69 69", "password123", "30-04-2006", 19);
+            Console.WriteLine(amir.Email);
         }
     }
 }
---
+++ AdminProgramProject/User.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminProgramLessons
+{
+    internal class User
+    {
+        private int _id;
+        private string _name;
+        private string _address;
+        private string _email;
+        private string _telNum;
+        private string _passWord;
+        private string _dOB; // date of birth
+        private int _age;
+
+
+        public string Email
+        {
+            get { return _email; }
+            set { _email = value; }
+        }
+
+
+
+
+        public User(int id, string name, string address, string email, string telNum, string password, string dOB, int age)
+        {
+            // _id = id;
+            _name = name;
+            // _address = address;
+            _email = email;
+            // _telNum = telNum;
+            // _passWord = password;
+            // _dOB = dOB;
+            // _age = age;
+        }
+
+        public void SetEmail(string email)
+        {
+            if (email.Contains("@gmail.com"))
+            {
+                _email = email;
+            }
+            else
+            {
+                Console.WriteLine("ERROR DIT IS GEEN GELDIG EMAIL ADDRESS");
+            }
+        }
+
+
+        public string GetEmail()
+        {
+            return _email;
+        }
+
+    }
+}
