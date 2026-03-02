dff07095e91c10695bf982a9cab7755d81a5c92e
Lychee304 <2173013@talnet.nl>
2026-03-02 12:42:07 +0100

    made the feature userClass branch

--- AdminProgramProject/Program.cs
+++ AdminProgramProject/Program.cs
@@ -16,7 +16,7 @@ namespace AdminProgramLessons
             User amir = new User(0, "amir", "naast de buren", "hey@gmail.com", "+31 12 34 56 78", "password123", "30-04-2006", 19);
             Console.WriteLine(amir.Email);
 
-            User user2 = new User(1, "name", "at home", "heyya@gmail.com", "+31 03 69 12 15", "wordpass", "01-01-1990", 36);
+            User user2 = new User(1, "name", "at home", "heyyya@gmail.com", "+31 03 69 12 15", "wordpass", "01-01-1990", 36);
             Console.WriteLine(user2.Email);
         }
     }
