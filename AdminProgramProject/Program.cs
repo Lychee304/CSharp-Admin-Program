671e8a57438e1a202cabba39bc5a3e9c4064c360
Lychee304 <2173013@talnet.nl>
2026-03-11 11:41:08 +0100

    unknown error searching
    no files has a error, yet the code does?

--- AdminProgramProject/Program.cs
+++ AdminProgramProject/Program.cs
@@ -10,14 +10,12 @@ namespace AdminProgramLessons
     {
         static void Main(string[] args)
         {
-<<<<<<< Updated upstream
 
             User amir = new User(0, "amir", "naast de buren", "hey@gmail.com", "+31 12 34 56 78", "password123", "30-04-2006", 19);
             Console.WriteLine(amir.Email);
 
             User user2 = new User(1, "name", "at home", "heyyya@gmwoail.com", "+31 03 69 12 15", "123wordpass", "01-01-1990", 36);
             Console.WriteLine(user2.Email);
-=======
             Console.WriteLine("naam: ");
             string _input = Console.ReadLine();
 
@@ -42,8 +40,18 @@ namespace AdminProgramLessons
             _input7 = 2025 - _input7;
 
             User user1 = new User(0, _input, _input2, _input3, _input4, _input5, _input6, _input7);
-            user1.WriteAll();
->>>>>>> Stashed changes
+
+            Console.WriteLine("wilt u de nieuwe aangemaakte klant zien? J = ja, N = nee");
+            string _askView = Console.ReadLine();
+
+
+            if (_askView == "J")
+            {
+                user1.WriteAll();
+            } else if (_askView == "N")
+            {
+                Console.WriteLine(_input + " is toegevoegd!");
+            }
 
 
 
