ab0362df9afc6d1786ad0e8e292e8826e90fe066
Lychee304 <2173013@talnet.nl>
2026-03-12 11:15:49 +0100

    mid work backup
    it has a error which prevents the while loop to run

--- AdminProgramProject/Program.cs
+++ AdminProgramProject/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,47 +9,66 @@ namespace AdminProgramLessons
 {
     internal class Program
     {
+
+        private bool _turningOn = false;
+        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("naam: ");
-            string _input = Console.ReadLine();
 
-            Console.WriteLine("address: ");
-            string _input2 = Console.ReadLine();
+            Console.WriteLine("zet het programma aan? J = ja, N = nee: ");
+            string _onOff = Console.ReadLine();
 
-            Console.WriteLine("email: ");
-            string _input3 = Console.ReadLine();
-
-            Console.WriteLine("telefoon nummer: ");
-            string _input4 = Console.ReadLine();
-
-            Console.WriteLine("wachtwoord: ");
-            string _input5 = Console.ReadLine();
-
-            Console.WriteLine("geboortedatum dd/mm: ");
-            string _input6 = Console.ReadLine();
-
-            Console.WriteLine("geboortejaar: ");    
-            int _input7 = int.Parse(Console.ReadLine());
-
-            _input7 = 2025 - _input7;
-
-            User user1 = new User(0, _input, _input2, _input3, _input4, _input5, _input6, _input7);
-
-            Console.WriteLine("wilt u de nieuwe aangemaakte klant zien? J = ja, N = nee");
-            string _askView = Console.ReadLine();
-
-
-            if (_askView == "J")
+            if (_onOff == "J")
             {
-                user1.WriteAll();
-            } else if (_askView == "N")
+                _turningOn = true;
+            }
+            else if (_onOff == "N")
             {
-                Console.WriteLine(_input + " is toegevoegd!");
+                Console.WriteLine("programma staat uit, fijne dag!");
             }
 
+            while (_turningOn == true)
+            {
+                Console.WriteLine("naam: ");
+                string _input = Console.ReadLine();
+
+                Console.WriteLine("address: ");
+                string _input2 = Console.ReadLine();
+
+                Console.WriteLine("email: ");
+                string _input3 = Console.ReadLine();
+
+                Console.WriteLine("telefoon nummer: ");
+                string _input4 = Console.ReadLine();
+
+                Console.WriteLine("wachtwoord: ");
+                string _input5 = Console.ReadLine();
+
+                Console.WriteLine("geboortedatum dd/mm: ");
+                string _input6 = Console.ReadLine();
+
+                Console.WriteLine("geboortejaar: ");
+                int _input7 = int.Parse(Console.ReadLine());
+
+                _input7 = 2025 - _input7;
+
+                User user1 = new User(0, _input, _input2, _input3, _input4, _input5, _input6, _input7);
+
+                Console.WriteLine("wilt u de nieuwe aangemaakte klant zien? J = ja, N = nee");
+                string _askView = Console.ReadLine();
 
 
+                if (_askView == "J")
+                {
+                    user1.WriteAll();
+                } else if (_askView == "N")
+                {
+                    Console.WriteLine(_input + " is toegevoegd!");
+                }
+            }
         }
+
+        
     }
 }
