f8d9e69c961992d562c53e120d7fa8ce2d72f3c6
Lychee304 <2173013@talnet.nl>
2026-03-12 11:45:40 +0100

    it works correctly now
    it just changes the information of 'user1' but it works!

--- AdminProgramProject/Program.cs
+++ AdminProgramProject/Program.cs
@@ -10,7 +10,7 @@ namespace AdminProgramLessons
     internal class Program
     {
 
-        private bool _turningOn = false;
+        //  bool _turningOn = false;
         
 
         static void Main(string[] args)
@@ -19,54 +19,60 @@ namespace AdminProgramLessons
             Console.WriteLine("zet het programma aan? J = ja, N = nee: ");
             string _onOff = Console.ReadLine();
 
+            bool _turningOn = false;
+
             if (_onOff == "J")
             {
                 _turningOn = true;
-            }
-            else if (_onOff == "N")
+            } else if (_onOff == "N")
             {
-                Console.WriteLine("programma staat uit, fijne dag!");
+                Console.WriteLine("ok, fijne dag!");
+            } else
+            {
+                Console.WriteLine("J = ja, N = nee");
             }
 
-            while (_turningOn == true)
-            {
-                Console.WriteLine("naam: ");
-                string _input = Console.ReadLine();
-
-                Console.WriteLine("address: ");
-                string _input2 = Console.ReadLine();
-
-                Console.WriteLine("email: ");
-                string _input3 = Console.ReadLine();
-
-                Console.WriteLine("telefoon nummer: ");
-                string _input4 = Console.ReadLine();
-
-                Console.WriteLine("wachtwoord: ");
-                string _input5 = Console.ReadLine();
-
-                Console.WriteLine("geboortedatum dd/mm: ");
-                string _input6 = Console.ReadLine();
-
-                Console.WriteLine("geboortejaar: ");
-                int _input7 = int.Parse(Console.ReadLine());
-
-                _input7 = 2025 - _input7;
-
-                User user1 = new User(0, _input, _input2, _input3, _input4, _input5, _input6, _input7);
-
-                Console.WriteLine("wilt u de nieuwe aangemaakte klant zien? J = ja, N = nee");
-                string _askView = Console.ReadLine();
-
-
-                if (_askView == "J")
+                while (_turningOn == true)
                 {
-                    user1.WriteAll();
-                } else if (_askView == "N")
-                {
-                    Console.WriteLine(_input + " is toegevoegd!");
+
+                    Console.WriteLine("naam: ");
+                    string _input = Console.ReadLine();
+
+                    Console.WriteLine("address: ");
+                    string _input2 = Console.ReadLine();
+
+                    Console.WriteLine("email: ");
+                    string _input3 = Console.ReadLine();
+
+                    Console.WriteLine("telefoon nummer: ");
+                    string _input4 = Console.ReadLine();
+
+                    Console.WriteLine("wachtwoord: ");
+                    string _input5 = Console.ReadLine();
+
+                    Console.WriteLine("geboortedatum dd/mm: ");
+                    string _input6 = Console.ReadLine();
+
+                    Console.WriteLine("geboortejaar: ");
+                    int _input7 = int.Parse(Console.ReadLine());
+
+                    _input7 = 2025 - _input7;
+
+                    User user1 = new User(0, _input, _input2, _input3, _input4, _input5, _input6, _input7);
+
+                    Console.WriteLine("wilt u de nieuwe aangemaakte klant zien? J = ja, N = nee");
+                    string _askView = Console.ReadLine();
+
+
+                    if (_askView == "J")
+                    {
+                        user1.WriteAll();
+                    }
+                    else if (_askView == "N")
+                    {
+                        Console.WriteLine(_input + " is toegevoegd!");
+                    }
                 }
-            }
         }
 
         
