6e271f1a92f40b450203d203dac617dafba3a8a8
Lychee304 <2173013@talnet.nl>
2026-03-19 10:28:59 +0100

    mid work backup
    it wont accept the list i made because it cant find it

--- AdminProgramProject/Program.cs
+++ AdminProgramProject/Program.cs
@@ -8,6 +8,16 @@ namespace AdminProgramLessons
 {
     internal class Program
     {
+<<<<<<< Updated upstream
+=======
+
+        //  bool _turningOn = false;
+
+
+        private static int _countingID = 0;
+        public string _userName = "User" + _countingID;
+
+>>>>>>> Stashed changes
         static void Main(string[] args)
         {
             Console.WriteLine("naam: ");
@@ -48,7 +58,64 @@ namespace AdminProgramLessons
             amir.WriteAll();
 
 
+<<<<<<< Updated upstream
 
+>>>>>>> Stashed changes
+=======
+
+                    _input7 = 2025 - _input7;
+
+                    User _userName = new User(0, _input, _input2, _input3, _input4, _input5, _input6, _input7); // with dynamic naming (example: user + _idCounter) it can probably make multiple users :D )
+
+                    AddUserToDBase();
+
+                    Console.WriteLine("wilt u de nieuwe aangemaakte klant zien? J = ja, N = nee");
+                    string _askView = Console.ReadLine();
+
+
+                    if (_askView == "J")
+                    {
+                        _userName.WriteAll();
+                    }
+                    else if (_askView == "N")
+                    {
+                        Console.WriteLine(_input + " is toegevoegd met nummer " + _countingID + "!");
+                    }
+
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("foutmelding 0001!, weet u zeker dat de geboortedatum in cijfers zijn ingevoerd? bijv: 1990 ipv negentien negentig");
+                }
+
+                Console.WriteLine("wil je de informatie veranderen of afsluiten? N = Nieuw persoon, A = Afsluiten, C = Checken voor een aangemaakte gebruiker");
+                string _askProg = Console.ReadLine(); // prog = progress
+
+
+                if (_askProg == "C")
+                {
+                    Console.WriteLine("ok, toets de nummer in van de gebruiker: ");
+                    int _checkUser = int.Parse(Console.ReadLine()); // 'Convert.' is the answer.. how tho
+
+
+                    User _checkUser = ShowUserWID(_countingID);  // _checkUser should be var type User, not string
+
+
+                } else if (_askProg == "A")
+                { 
+                    Console.WriteLine("ok, fijne dag!");
+                    _turningOn = false;
+
+
+                } else if (_askProg == "N")
+                {
+                    _countingID++;
+
+                } else
+                {
+                    Console.WriteLine("appel");
+                }
+            }
 >>>>>>> Stashed changes
         }
     }
--- AdminProgramProject/UserDatabase.cs
+++ AdminProgramProject/UserDatabase.cs
@@ -8,6 +8,23 @@ namespace AdminProgramProject
 {
     internal class UserDatabase
     {
+<<<<<<< Updated upstream
         
+=======
+        // base for the user data
+        // data of the user
+        // user of the data
+        // wow, such poetry
+        // alright cya later!
+
+        public static List<User> Users = new List<User>();
+
+
+        public void AddUserToDBase()   // DBase = data base
+        {
+            Users.add(_userName);
+        }
+>>>>>>> Stashed changes
     }
 }
+
